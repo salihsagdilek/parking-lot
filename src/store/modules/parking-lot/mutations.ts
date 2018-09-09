9870d2be8cabc2035c310ea6d5f75e5261a3ace4
Salih Sagdilek <salihsagdilek@gmail.com>
2018-09-09 17:09:57 +0300

    update grid keep existing slots

--- src/store/modules/parking-lot/mutations.ts
+++ src/store/modules/parking-lot/mutations.ts
@@ -4,7 +4,13 @@ import {Slot, Settings} from './types';
 
 export const mutations: MutationTree<ParkingLotState> = {
   updateGrids(state, payload: Slot[]) {
-    state.slots = payload;
+    state.slots = payload.map((slot: Slot) => {
+      const slotExist = state.slots.find((s: Slot) => s.code === slot.code);
+      if (slotExist) {
+        slot = slotExist;
+      }
+      return slot;
+    });
   },
   updateSettings(state, payload: Settings) {
     state.settings = payload;
